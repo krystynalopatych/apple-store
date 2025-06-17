ef736421c9f788c71d2f4e9862ac046f783c7f9e
krystynalopatych <krystyna.lopatych@gmail.com>
2025-06-17 23:46:40 +0300

    Website version with all styles

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="{{ asset('styles/registerstyle.css') }}?v={{ time() }}">
     <title>{{ $mode == 'register' ? 'Registration' : 'Login' }}</title>
 </head>
 <body>
